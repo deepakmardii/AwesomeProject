651a2ab43a02cb28ab84cdcc507ad5932640542b
Deepak Mardi <deepakmardi22@gmail.com>
2024-03-21 21:17:03 +0530

    Simple Counter App

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View,Button } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0)
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>you clicked : {count}</Text>
+      <Button
+        onPress={() => setCount(count + 1)}
+        title="Click me!"
+      />
     </View>
   );
 }
